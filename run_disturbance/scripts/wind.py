4749fc244f6a79e3570252dba993325db49b62cd
kjells <kjell.swedin@gmail.com>
2017-05-26 12:33:25 -0700

    add sound_wood_loadings() to wind

--- run_disturbance/scripts/wind.py
+++ run_disturbance/scripts/wind.py
@@ -183,20 +183,66 @@ scale_these = {
             
 def placeholder(fb):
     pass
+    
+def sound_wood_loadings(fb, multiplier, min1, min2, min3):
+    def is_empty():
+        total = 0.0
+        ids = [
+            libfbrw.FBTypes.eWOODY_FUEL_SOUND_WOOD_LOADINGS_ZERO_TO_THREE_INCHES_ONE_TO_THREE_INCHES,
+            libfbrw.FBTypes.eWOODY_FUEL_SOUND_WOOD_LOADINGS_ZERO_TO_THREE_INCHES_QUARTER_INCH_TO_ONE_INCH,
+            libfbrw.FBTypes.eWOODY_FUEL_SOUND_WOOD_LOADINGS_ZERO_TO_THREE_INCHES_ZERO_TO_QUARTER_INCH,
+            libfbrw.FBTypes.eWOODY_FUEL_SOUND_WOOD_LOADINGS_GREATER_THAN_THREE_INCHES_THREE_TO_NINE_INCHES,
+            libfbrw.FBTypes.eWOODY_FUEL_SOUND_WOOD_LOADINGS_GREATER_THAN_THREE_INCHES_NINE_TO_TWENTY_INCHES,
+            libfbrw.FBTypes.eWOODY_FUEL_SOUND_WOOD_LOADINGS_GREATER_THAN_THREE_INCHES_GREATER_THAN_TWENTY_INCHES
+        ]
+        for i in ids:
+            value = fb.GetValue(i)
+            try:
+                total += float(value)
+            except:
+                pass
+        return total > 0
+
+    def check_and_set(fb, os, ms, check_value, id, multiplier, min):
+        if os > check_value or ms > check_value:
+            fbrw.scale(fb, id, multiplier, min)
+        else:
+            fb.SetValue(id, "")
+        
+    os_diameter = fb.GetValue(libfbrw.FBTypes.eCANOPY_TREES_OVERSTORY_DIAMETER_AT_BREAST_HEIGHT)
+    os_diameter = float(os_diameter) if len(os_diameter) else 0.0
+    ms_diameter = fb.GetValue(libfbrw.FBTypes.eCANOPY_TREES_MIDSTORY_DIAMETER_AT_BREAST_HEIGHT)
+    ms_diameter = float(ms_diameter) if len(ms_diameter) else 0.0
+    
+    check_and_set(fb, os_diameter, ms_diameter, 3,
+        libfbrw.FBTypes.eWOODY_FUEL_SOUND_WOOD_LOADINGS_GREATER_THAN_THREE_INCHES_THREE_TO_NINE_INCHES,
+        multiplier, 'min={}'.format(min1))
+    check_and_set(fb, os_diameter, ms_diameter, 9,
+        libfbrw.FBTypes.eWOODY_FUEL_SOUND_WOOD_LOADINGS_GREATER_THAN_THREE_INCHES_NINE_TO_TWENTY_INCHES,
+        multiplier, 'min={}'.format(min2))
+    check_and_set(fb, os_diameter, ms_diameter, 20,
+        libfbrw.FBTypes.eWOODY_FUEL_SOUND_WOOD_LOADINGS_GREATER_THAN_THREE_INCHES_GREATER_THAN_TWENTY_INCHES,
+        multiplier, 'min={}'.format(min3))
+    
+    if is_empty():
+        fb.SetNodeEmpty(libfbrw.FBNodeWithSpeciesType.eWOODY_FUEL_SOUND_WOOD_ALL_SOUND_WOOD)
+        
+        
+ 
   
 special_funcs = {
     fbrw.SEVERITY[0]: {
-        fbrw.TIMESTEP[0]: [ (placeholder,) ],
+        fbrw.TIMESTEP[0]: [ (sound_wood_loadings, 2, 0, 0, 0) ],
         fbrw.TIMESTEP[1]: [ (placeholder,), (sp.process_sound_wood, 0.25)],
         fbrw.TIMESTEP[2]: [ (placeholder,), (sp.process_sound_wood, 0.5)],
     },
     fbrw.SEVERITY[1]: {
-        fbrw.TIMESTEP[0]: [ (placeholder,) ],
+        fbrw.TIMESTEP[0]: [ (sound_wood_loadings, 2.5, 3, 6.5, 7.5) ],
         fbrw.TIMESTEP[1]: [ (placeholder,), (sp.process_sound_wood, 0.25)],
         fbrw.TIMESTEP[2]: [ (placeholder,), (sp.process_sound_wood, 0.5)],
     },
     fbrw.SEVERITY[2]: {
-        fbrw.TIMESTEP[0]: [ (placeholder,) ],
+        fbrw.TIMESTEP[0]: [ (sound_wood_loadings, 4, 3, 6.5, 7.5) ],
         fbrw.TIMESTEP[1]: [ (placeholder,), (sp.process_sound_wood, 0.25)],
         fbrw.TIMESTEP[2]: [ (placeholder,), (sp.process_sound_wood, 0.5)],
     }
