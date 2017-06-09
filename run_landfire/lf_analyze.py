bb2991f4993f97725197cb29517e70e9e4049119
kjells <kjell.swedin@gmail.com>
2017-06-09 13:14:26 -0700

    run analysis script across all disturbances

--- run_landfire/lf_analyze.py
+++ run_landfire/lf_analyze.py
@@ -4,6 +4,18 @@ import re
 
 # Fire across severities at the first timestep
 FIRE_ALLSEVERITY_TS1 = '\d+_1.1$'
+MECHADD_ALLSEVERITY_TS1 = '\d+_2.1$'
+MECHREMOVE_ALLSEVERITY_TS1 = '\d+_3.1$'
+WIND_ALLSEVERITY_TS1 = '\d+_4.1$'
+INSECTS_ALLSEVERITY_TS1 = '\d+_5.1$'
+
+DISTURBANCES = [
+    FIRE_ALLSEVERITY_TS1,
+    MECHADD_ALLSEVERITY_TS1,
+    MECHREMOVE_ALLSEVERITY_TS1,
+    WIND_ALLSEVERITY_TS1,
+    INSECTS_ALLSEVERITY_TS1
+]
 
 # ========================================================================================
 #  Returns a True|False mask array. Use to pick fuelbeds based on disturbance.
@@ -12,14 +24,14 @@ FIRE_ALLSEVERITY_TS1 = '\d+_1.1$'
 def get_mask(df, regex):
     return [True if j else False for j in (re.match(regex, i) for i in df.Fuelbed_number)]
 
-def write_group_csv(gb):
+def write_group_csv(gb, dist_id):
     df_out = pd.DataFrame()
     sgroups = sorted([int(i) for i in gb.groups])
     for g in sgroups:
         df_out = df_out.append(gb.get_group(str(g))[['Fuelbed_number','Fuelbed_name', 'Benchmark_ROS','Total_aboveground_biomass']])
-    df_out.to_csv('fire_ros_biomass.csv', index=False)    
+    df_out.to_csv('ros_biomass_{}.csv'.format(dist_id), index=False)    
 
-def build_visualizer(gb, cols_values):
+def build_visualizer(gb, cols_values, dist_id):
     df_means = pd.DataFrame()
     for c in cols_values: 
         s1 = gb.nth([0]).get(c).mean().round(2)
@@ -28,32 +40,34 @@ def build_visualizer(gb, cols_values):
         arrow = '='    # equals
         if s1 > s2 and s2 > s3:
             arrow = '&darr;'
-            #arrow = '↓'
         if s1 < s2 and s2 < s3:
             arrow = '&uarr;'
-            #arrow = '↑'
             
         line = '{},{},{},{},{}'.format(arrow, c, s1, s2, s3)
         #print(line)
         df_means = df_means.append({'Trend':arrow, 'Name':c, 'S1_mean':s1, 'S2_mean':s2, 'S3_mean':s3}, ignore_index=True)
 
     df_means = df_means[['Trend','Name','S1_mean','S2_mean','S3_mean']]
-    from IPython.display import display, HTML
-    df_means.to_html('means.html', index=False, escape=False)
+    from IPython.display import HTML
+    df_means.to_html('means_{}.html'.format(dist_id), index=False, escape=False)
 
 def process(filename):
     df = pd.read_csv(filename)
+    df.fillna(0, inplace=True)
 
     # get the non-empty, numeric column names
     cols_values = df.columns.values
     cols_values = np.delete(cols_values, [0,1,2,16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,len(cols_values)-1])
 
-    mask = get_mask(df, FIRE_ALLSEVERITY_TS1)
-    dfire = df.loc[mask]
-    gb = dfire.groupby(by=dfire.Fuelbed_number.apply(lambda x: x.split('_')[0]))
-    write_group_csv(gb)
+    for disturbance in DISTURBANCES:
+        dist_id = disturbance.split('_')[1][0]
+        print('Dist id is "{}"'.format(dist_id))
+        mask = get_mask(df, disturbance)
+        df_tmp = df.loc[mask]
+        gb = df_tmp.groupby(by=df_tmp.Fuelbed_number.apply(lambda x: x.split('_')[0]))
+        write_group_csv(gb, dist_id)
 
-    build_visualizer(gb, cols_values)
+        build_visualizer(gb, cols_values, dist_id)
 
 # ========================================================================================
 #  Start
