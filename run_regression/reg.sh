1847009feb147ff323da4fdec00c78522806f239
Brian Drye <bridrye@gmail.com>
2018-06-19 12:28:42 -0700

    update python command to python3

--- run_regression/reg.sh
+++ run_regression/reg.sh
@@ -2,6 +2,6 @@
 
 rm e_*.txt
 
-for a in fire mechadd mechremove wind insects; do python regress.py $a > e_$a.txt; done
+for a in fire mechadd mechremove wind insects; do python3 regress.py $a > e_$a.txt; done
 
 for a in fire mechadd mechremove wind insects; do echo $a && tail -3 e_$a.txt; done
